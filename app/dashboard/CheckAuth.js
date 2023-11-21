fe78e411be6c5d88fbcccd13f95b3c74f1ff1f60
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-11-21 16:23:42 -0500

    Check auth

--- app/dashboard/CheckAuth.js
+++ app/dashboard/CheckAuth.js
@@ -1,5 +1,19 @@
+"use client"
+import { useEffect, useContext } from "react"
+import { useRouter } from "next/navigation"
+import { AuthContext } from "@/context/AuthContext"
 export default function CheckAuth(){
     
+    const {user} = useContext(AuthContext)
+
+    const router = useRouter()
+
+    useEffect(() => {
+        if(!user){
+            // rederict to login page
+            router.push('/login')
+        }
+    }, [user])
     
     
     return null
